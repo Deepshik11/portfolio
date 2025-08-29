cc5559a3d036ca17873ab3dc16f094e1b72a0195
Deepshik11 <deepdeepshik500@gmail.com>
2025-08-29 22:10:25 +0530

    new vist

--- frontend/src/utils/visitorTracker.js
+++ frontend/src/utils/visitorTracker.js
@@ -1,11 +1,12 @@
+// utils/trackVisitor.js
 export const trackVisitor = async (section) => {
   try {
     await fetch("https://portfolio-rjdm.vercel.app/api/visitors", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        section: section,   // e.g. "home", "projects", "about"
-        timestamp: new Date(),
+        section,   // shorthand for section: section
+        timestamp: new Date().toISOString(), // always send ISO string for consistency
       }),
     });
   } catch (error) {
